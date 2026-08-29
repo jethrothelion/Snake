5eb9aca2d5adb0b3b9851698122f452218308d96
jethr <jethrothelion@gmail.com>
2026-08-29 14:35:24 -0400

    Basic start have display

--- Main.c
+++ Main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
+    char display[5][20];
 
+
+    memset(display, '*', sizeof(display));
+
+    while(1)
+    {
+
+        //print Display
+        for(int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 20; j++)
+            {
+                printf("%c ", display[i][j]);
+            }
+            printf("\n");
+        }
+
+        //clear Display
+        printf("\033[5A");
+    }
 }
---
+++ readme.md
@@ -0,0 +1,3 @@
+Standard snake writin in c
+
+Just a learning project to learn C. no AI, all documentation and googling 
