5a686f3315f56b7991a3e65a1895e95725d6c9e5
jethrothelion <150306570+jethrothelion@users.noreply.github.com>
2026-09-01 18:10:46 -0400

    Further correct method signatures, and other ;p;

--- Main.c
+++ Main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
 int randomNum(int range, int takenNums[], int takenCount);
-void addElementToArray(int target, int *array, int *counter);
+void addElementToArray(int target, int array[], int *counter);
+int currentPosition;
 
 int main(void)
 {
@@ -17,20 +20,23 @@ int main(void)
     memset(display, '*', sizeof(display));
 
 
+    currentPosition = 0;
+
+    point = randomNum(100, takenNums, takenNumCounter);
+    addElementToArray(point, takenNums, &takenNumCounter);
+
+    spawnPoint = randomNum(100, takenNums, takenNumCounter);
+
+
     while(1)
     {
-        addElementToArray(randomNum(100)
-        spawnPoint = randomNum(100);
-        charecter[]
-
-
-        point = randomNum(100);
 
         //print Display
         for(int i = 0; i < 5; i++)
         {
             for (int j = 0; j < 20; j++)
             {
+                currentPosition += 1;
                 printf("%c ", display[i][j]);
             }
             printf("\n");
@@ -39,35 +45,35 @@ int main(void)
         //clear Display
         printf("\033[5A");
     }
-
-    int randomNum(int range, int[] takenNums)
-    //Give random number in a range of numbers
-    {
-
-        bool aDuplicate;
-        int theRandomNum
-
-        do
-        {
-            int theRandomNum = min + rand() % (max - min + 1);
-            aDuplicate = false;
-
-            for (int i = 0; i < takenCount; i++)
-            {
-                if (takenNums[i] == theRandomNum)
-                {
-                    aDuplicate = true;
-                    break;
-                }
-            }
-        } while(aDuplicate);
-
-        return theRandomNum;
-    }
-
-    void addElementToArray(int target, int[] *array, int *counter)
-    {
-        *counter =+ 1;
-        *array[counter] = target;
-    }
 }
+int randomNum(int range, int takenNums[], int takenCount)
+//Give random number in a range of numbers
+{
+
+    bool aDuplicate;
+    int theRandomNum;
+
+    do
+    {
+        theRandomNum = rand() % range;
+        aDuplicate = false;
+
+        for (int i = 0; i < takenCount; i++)
+        {
+            if (takenNums[i] == theRandomNum)
+            {
+                aDuplicate = true;
+                break;
+            }
+        }
+    } while(aDuplicate);
+
+    return theRandomNum;
+}
+
+void addElementToArray(int target, int array[], int *counter)
+{
+    array[*counter] = target;
+    *counter += 1;
+}
+
